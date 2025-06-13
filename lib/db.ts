84ce1d23965f0bdf328672beda4f576ca84e6b1e
Mochiizo <Promathis.hautbois@gmail.com>
2025-06-13 11:06:49 +0200

    feat(db): mise à jour de la configuration de la base de données pour utiliser une chaîne de connexion sécurisée
    - Remplacement des paramètres de connexion par une chaîne d'environnement DATABASE_URL.
    - Ajout de la gestion SSL pour les connexions en production.

--- lib/db.ts
+++ lib/db.ts
@@ -5,11 +5,8 @@ import { Pool } from 'pg';
  * Utilise un pool de connexions pour gérer efficacement les connexions multiples
  */
 const pool = new Pool({
-  user: 'postgres',      // Nom d'utilisateur de la base de données
-  host: 'localhost',     // Hôte de la base de données
-  database: 'DB_READING_TRACKER', // Nom de la base de données
-  password: 'postgres',  // Mot de passe (à remplacer par une valeur sécurisée en production)
-  port: 5432,           // Port par défaut de PostgreSQL
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 // Export du pool de connexions pour être utilisé dans toute l'application
